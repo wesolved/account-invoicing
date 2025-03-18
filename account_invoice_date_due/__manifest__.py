38d8bd5eef2f3c8ac9e2d0d9fc762e69d80fa690
rkabranov <rkabranov@wesolved.com>
2025-03-18 12:26:12 +0100

    [MIG] account_invoice_date_due: Migration to 18.0

---
+++ account_invoice_date_due/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Update Invoice's Due Date",
+    "version": "18.0.1.0.0",
+    "author": "Vauxoo, Odoo Community Association (OCA)",
+    "category": "Accounting",
+    "website": "https://github.com/OCA/account-invoicing",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "security/account_security.xml",
+        "security/ir.model.access.csv",
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+    "development_status": "Production/Stable",
+    "maintainers": ["luisg123v", "joao-p-marques"],
+} 
